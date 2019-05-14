cbb99ec9d652fa8bb6631c343e8942247bc689fb
Bruno Alves Germano Pinto <bruno.pinto@S4B01046.sbd.corp>
2019-05-14 09:20:06 -0300

    Get my user on saga

--- src/components/GithubApp/sagas.js
+++ src/components/GithubApp/sagas.js
@@ -3,9 +3,10 @@ import CallApi from '../CallApi';
 import { GITHUB_APP_REQUEST, GITHUB_APP_SUCCESS, GITHUB_APP_FAILURE } from './actions';
 
 export function* GithubRequestSaga(params) {
-
+  console.log('de boas na saga')
   try {
-    const GithubCall = yield call(CallApi, 'get', '');
+    const GithubCall = yield call(CallApi, 'get', 'https://api.github.com/users/brgermano');
+    console.log('data', GithubCall.data)
     const GithubData = GithubCall ? GithubCall.data : false;
 
     yield put({
