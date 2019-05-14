e6f18e5b0434b3808fb4995fcd67c82d41e20ca0
Bruno Alves Germano Pinto <bruno.pinto@S4B01046.sbd.corp>
2019-05-14 09:25:45 -0300

    Set config.json on call api component

--- src/components/CallApi/index.js
+++ src/components/CallApi/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import config from '../../../src/config.json';
 
-function* CallApi(method, url, data, type, responsetype) {
+function* CallApi(method, endpoint, data, type, responsetype) {
+  const { apiEndpoint } = config;
   const formData = new FormData();
   const responseType = responsetype === undefined ? 'json' : responsetype;
   const contentType = type === undefined ? 'application/json' : 'multipart/form-data';
   const files = Array.isArray(data) ? data.map(item => formData.append('file', item.file)) : formData.append('file', data);
   const bodyRequest = contentType === 'application/json' ? data : files;
 
-  const call = yield axios(url, {
+  const call = yield axios(`${apiEndpoint}${endpoint}`, {
     method,
     data: contentType === 'application/json' ? bodyRequest : formData,
     headers: {
--- src/components/GithubApp/sagas.js
+++ src/components/GithubApp/sagas.js
@@ -5,7 +5,7 @@ import { GITHUB_APP_REQUEST, GITHUB_APP_SUCCESS, GITHUB_APP_FAILURE } from './ac
 export function* GithubRequestSaga(params) {
   console.log('de boas na saga')
   try {
-    const GithubCall = yield call(CallApi, 'get', 'https://api.github.com/users/brgermano');
+    const GithubCall = yield call(CallApi, 'get', '/users/brgermano');
     console.log('data', GithubCall.data)
     const GithubData = GithubCall ? GithubCall.data : false;
 
